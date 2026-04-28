c591fbc4fd189d1ece2bde000f9ead89007b3164
Amir Marmul <amir@marmul.my.id>
2026-04-28 13:00:19 +0800

    feat(account): implement password reset functionality
    - Add python-decouple to manage environment variables.
    - Configure SMTP email settings for sending password reset links.
    - Implement password reset views and URL patterns.
    - Add a 'forgot password' link to the login page.
    - Create all necessary templates for the password reset flow.

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,6 +118,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Django Auth
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+# Email server configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
